9ca2df4ee306b4125734e6e2f1228fa42e4536f1
imomushibikenyan <yukariniwatorinotameni@outlook.jp>
2024-07-15 03:08:09 +0900

    userモデルバリ・ビューファイル名前訂正

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # バリデーション
+  validates :nickname, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :last_name, presence: true, format: {  with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :first_name, presence: true, format: {  with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :birth_date, presence: true
+
+end
