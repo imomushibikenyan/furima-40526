693afc3f2d237a3affbd08163de2551a375cbb23
imomushibikenyan <yukariniwatorinotameni@outlook.jp>
2024-07-25 01:07:21 +0900

    出品機能実装／エラハン諦め

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,15 +8,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_day
   has_one_attached :image
 
-  with_options presence: true do
-    validates :name
-    validates :description
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    validates :image
-    validates :category_id, numericality: { other_than: 1 }
-    validates :condition_id, numericality: { other_than: 1 }
-    validates :shipping_id, numericality: { other_than: 1 }
-    validates :region_id, numericality: { other_than: 1 }
-    validates :shipping_day_id, numericality: { other_than: 1 }
-  end
+  validates :name, presence: { message: "を入力してください" }
+  validates :description, presence: { message: "を入力してください" }
+  validates :price, presence: { message: "を入力してください" }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "は¥300〜¥9,999,999の間で入力してください" }
+  validates :image, presence: { message: "をアップロードしてください" }
+  validates :category_id, numericality: { other_than: 1, message: "は無効です" }, presence: { message: "を選択してください" }
+  validates :condition_id, numericality: { other_than: 1, message: "は無効です" }, presence: { message: "を選択してください" }
+  validates :shipping_id, numericality: { other_than: 1, message: "は無効です" }, presence: { message: "を選択してください" }
+  validates :region_id, numericality: { other_than: 1, message: "は無効です" }, presence: { message: "を選択してください" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "は無効です" }, presence: { message: "を選択してください" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
- root "items#index"
+  root "items#index"
+  resources :items, only: [:new, :create]
+
  
 end
