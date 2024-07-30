4cd1de8a00e48b838439012e5f714d0b038e868c
imomushibikenyan <yukariniwatorinotameni@outlook.jp>
2024-07-31 01:11:20 +0900

    indexアクションに訂正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,18 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: [:new, :create]
+  before_action :set_item, only: [:index, :create]
 
-  def new
+  def index
     @order_form = OrderForm.new(item_id: @item.id, user_id: current_user.id)
   end
   
-
   def create
     @order_form = OrderForm.new(order_form_params)
-
-    if @order_form.save
-      redirect_to root_path, notice: '購入が完了しました'
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
     else
-      render :new
+      render 'index', status: :unprocessable_entity
     end
   end
 
@@ -26,7 +25,7 @@ class OrdersController < ApplicationController
   def order_form_params
     params.require(:order_form).permit(
       :item_id, :user_id,
-      :postal_code, :recipient_phone, :region_id, :city, :street_address, :building_name
-    )
+      :postal_code, :recipient_phone, :region_id, :city, :street_address, :building_name, :token
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
-    resource :order, only: [:new, :create]
+    resources :orders, only: [:index, :create]
   end
 end
