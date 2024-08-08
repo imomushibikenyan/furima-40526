c51afaefe0c9ab8dd79451b43afc2c9d40478f42
imomushibikenyan <yukariniwatorinotameni@outlook.jp>
2024-08-09 01:36:36 +0900

    PAYJP環境変数設定済

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,15 +3,18 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_form = OrderForm.new(item_id: @item.id, user_id: current_user.id)
   end
   
   def create
     @order_form = OrderForm.new(order_form_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render 'index', status: :unprocessable_entity
     end
   end
@@ -24,8 +27,17 @@ class OrdersController < ApplicationController
 
   def order_form_params
     params.require(:order_form).permit(
-      :item_id, :user_id,
-      :postal_code, :recipient_phone, :region_id, :city, :street_address, :building_name, :token
-    ).merge(user_id: current_user.id, item_id: params[:item_id])
+    :postal_code, :recipient_phone, :region_id, :city, :street_address, :building_name
+    ).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      amount = @item.price
+      Payjp::Charge.create(
+        amount: amount,
+        card: @order_form.token,
+        currency: 'jpy'
+      )
   end
 end
