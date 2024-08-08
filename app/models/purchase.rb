2e5f62acfd7aaf72f2a5b41b926d44f75663ef5c
imomushibikenyan <yukariniwatorinotameni@outlook.jp>
2024-08-09 02:32:04 +0900

    データ保存失敗分を訂正

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,5 @@
 class Purchase < ApplicationRecord
-  belongs_to :user, foreign_key: 'buyer_id'
+  belongs_to :user, foreign_key: 'user_id'
   belongs_to :item
   has_one :shipping_address, dependent: :destroy
 
