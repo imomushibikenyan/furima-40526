8256b650e7d3f64d0872ed53077b3904e18eca43
imomushibikenyan <yukariniwatorinotameni@outlook.jp>
2024-08-12 16:41:34 +0900

    修正

--- db/migrate/20240730115110_create_purchases.rb
+++ db/migrate/20240730115110_create_purchases.rb
@@ -2,7 +2,7 @@ class CreatePurchases < ActiveRecord::Migration[7.0]
   def change
     create_table :purchases do |t|
       t.references :user, foreign_key: { to_table: :users }
-      t.references :item, foreign_key: true
+      t.references :product, null: false, foreign_key: { to_table: :products }
       t.timestamps
     end
   end
