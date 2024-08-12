956363a77162e0dac92cc5b909eb6fa2d5313473
imomushibikenyan <yukariniwatorinotameni@outlook.jp>
2024-08-12 17:56:22 +0900

    テーブル名間違い訂正

--- db/migrate/20240730115110_create_purchases.rb
+++ db/migrate/20240730115110_create_purchases.rb
@@ -2,7 +2,7 @@ class CreatePurchases < ActiveRecord::Migration[7.0]
   def change
     create_table :purchases do |t|
       t.references :user, null: false, foreign_key: { to_table: :users }
-      t.references :product, null: false, foreign_key: { to_table: :products }
+      t.references :product, null: false, foreign_key: { to_table: :items }
       t.timestamps
     end
   end
